1816f0ea33222b559b62df2a4e9d7d658cbc359e
Aadhithya01 <aadhithyaraja180@gmail.com>
2023-12-20 00:55:19 +0530

    Sixth

---
+++ Plotting.R
@@ -0,0 +1,5 @@
+# Load the iris dataset
+data(iris)
+
+# Create and display a histogram
+hist(iris$Sepal.Length, main = "Histogram of Sepal Length", xlab = "Sepal Length",ylab = "Frequency1", col = "lightyellow")
